f767bf8cc0d215ebc697bb640f8c14d0d8c8c6a8
zidniryi <zidnirydwan@gmail.com>
2025-04-06 18:10:06 +0700

    feat: update portofolio

---
+++ copyfiles.sh
@@ -0,0 +1,12 @@
+pwd
+# cd /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes
+cd  /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/
+mkdir public
+cp /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/index.html /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/public/
+cp /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/manifest.json /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/public/
+cp /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/serviceWorker.js /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/public/
+cp /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/404.html /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/public/
+cd  /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/public/
+mkdir assets
+cp -r /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/assets/* /Users/zidniryirawat/Documents/Code/portfolio-website-dark-light-themes/public/assets
+# cp ../index.html ../public/
